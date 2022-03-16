60db7578c18efdf419925ff7d18e94e4c3261bfa
chunservice <anson.it@yahoo.com>
2022-03-16 18:30:18 +1100

    Updates: - added a get route to add two numbers

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // using Express
 var express = require('express');
 
-// create an app express
+// create a variable called app app = express
 var app = express();
 
-// default path to the index.html in the public folder
+// default path of hosting to the public folder
 app.use(express.static(__dirname + '/public'));
 
 // set port = 3030
@@ -23,11 +23,13 @@ const addtwonum = function(n1,n2)
 console.log(addtwonum (10,14));
 
 // to get the function on server side, using REST API
+// response is what we see from the user, response is what we send back to the user
 app.get ("/addtwonum", function(req,res){
 
-    // define constant n1 and n2
+    // define constant n1 and n2, parse Integer if user input string, that will be an error as integer is required
     const n1=parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
     const result= addtwonum (n1,n2);
+    // res.json({statusCode: 200, data: result, message:"Success"})
     res.send('The summation result is: '+result);
 }) 
